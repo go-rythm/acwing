dfe55b56808953ff9d370e872a2850d6d266ed80
luxcgo <luxcgo@gmail.com>
2021-07-11 11:15:49 +0800

    0866: 质数判定

--- problems/0866. 试除法判定质数/0866. 试除法判定质数.go
+++ problems/0866. 试除法判定质数/0866. 试除法判定质数.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i <= n/i; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var x int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&x)
+		if isPrime(x) {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
+	}
 }
