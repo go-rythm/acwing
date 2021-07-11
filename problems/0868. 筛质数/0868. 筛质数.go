aba1f00de0c698d3e94849cbfa99509380e47ad2
luxcgo <luxcgo@gmail.com>
2021-07-11 16:54:19 +0800

    0868: 线性筛法求素数

--- problems/0868. 筛质数/0868. 筛质数.go
+++ problems/0868. 筛质数/0868. 筛质数.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+const N = 1000010
+
+var (
+	prime [N]int
+	st    [N]bool
+	cnt   int
+	x     int
+)
+
+func getPrimes(x int) {
+	for i := 2; i <= x; i++ {
+		if !st[i] {
+			prime[cnt] = i
+			cnt++
+		}
+		for j := 0; prime[j] <= x/i; j++ {
+			st[prime[j]*i] = true
+			if i%prime[j] == 0 {
+				break
+			}
+		}
+	}
+}
+
+func main() {
+	fmt.Scan(&x)
+	getPrimes(x)
+	fmt.Println(cnt)
 }
