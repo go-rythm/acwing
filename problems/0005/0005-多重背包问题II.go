62c5cc8ce4343690ff1a8fdd455d9fdccf70fef4
luxcgo <luxcgo@gmail.com>
2021-10-30 22:32:58 +0800

    0005: 多重背包时间优化版本加详细注解

--- problems/0005/0005-多重背包问题II.go
+++ problems/0005/0005-多重背包问题II.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var (
+		n, m int
+		v, w []int
+		g    []int
+	)
+
+	// in, err := os.Open("in.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer in.Close()
+	// os.Stdin = in
+
+	fmt.Scan(&n, &m)
+	v = make([]int, 11*n+1)
+	w = make([]int, 11*n+1)
+	g = make([]int, m+1)
+
+	index := 0
+	for i := 1; i <= n; i++ {
+		var vi, wi, si int
+		fmt.Scan(&vi, &wi, &si)
+		newItem := 1
+		var total int
+		for total = 1; total <= si; {
+			index++
+			v[index] = vi * newItem
+			w[index] = wi * newItem
+			newItem *= 2
+			total += newItem
+		}
+		total -= newItem
+		if total < si {
+			index++
+			v[index] = vi * (si - total)
+			w[index] = wi * (si - total)
+		}
+	}
+	for i := 1; i <= index; i++ {
+		for j := m; j >= v[i]; j-- {
+			if g[j-v[i]]+w[i] > g[j] {
+				g[j] = g[j-v[i]] + w[i]
+			}
+		}
+	}
+	fmt.Println(g[m])
 }
