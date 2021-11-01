0f9ba3bcc00f111f3330d51212de6a3b296591eb
luxcgo <luxcgo@gmail.com>
2021-11-02 01:20:40 +0800

    0895: 线性DP之最长上升子序列

--- problems/0895/0895-最长上升子序列.go
+++ problems/0895/0895-最长上升子序列.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+const N = 1010
+
+func main() {
+	var (
+		n    int
+		a, f [N]int
+	)
+
+	fmt.Scan(&n)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	for i := 1; i <= n; i++ {
+		f[i] = 1 // 只有a[i]一个数
+		for j := 1; j < i; j++ {
+			if a[j] < a[i] {
+				f[i] = Max(f[i], f[j]+1)
+			}
+		}
+	}
+
+	res := 0
+	for i := 1; i <= n; i++ {
+		res = Max(f[i], res)
+	}
+	fmt.Println(res)
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
