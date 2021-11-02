d3ad451f47d42a73fa11fa050308b1ca93de5567
luxcgo <luxcgo@gmail.com>
2021-11-03 01:46:47 +0800

    0897: 最长公共子序列

--- problems/0897/0897-最长公共子序列.go
+++ problems/0897/0897-最长公共子序列.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/luxcgo/go-acwing/internal/oj"
+)
+
+const (
+	N = 1010
+)
+
+var (
+	r    = bufio.NewReader(os.Stdin)
+	n, m int
+	a, b string
+	f    [N][N]int
+)
+
+func main() {
+	fmt.Fscan(r, &n, &m, &a, &b)
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if a[i-1] == b[j-1] {
+				f[i][j] = f[i-1][j-1] + 1
+			} else {
+				f[i][j] = oj.Max(f[i-1][j], f[i][j-1])
+			}
+		}
+	}
+
+	fmt.Println(f[n][m])
 }
