9f1ed84c99ab476ff2c853b5808fa76af006fc10
luxcgo <luxcgo@gmail.com>
2021-11-07 02:10:51 +0800

    0905,0908: 贪心之区间问题

--- problems/0905/0905-区间选点.go
+++ problems/0905/0905-区间选点.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
 
+type pair struct {
+	l int
+	r int
+}
+
+func main() {
+	// in := oj.In()
+	// os.Stdin = in
+	// defer in.Close()
+
+	in := bufio.NewReader(os.Stdin)
+
+	var cnt int
+	fmt.Scan(&cnt)
+	pairs := make([]*pair, cnt)
+	for i := 0; i < cnt; i++ {
+		p := &pair{}
+		fmt.Fscan(in, &p.l, &p.r)
+		pairs[i] = p
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].r < pairs[j].r
+	})
+	r := int(-2e9)
+	res := 0
+	for _, v := range pairs {
+		if v.l > r {
+			r = v.r
+			res++
+		}
+	}
+
+	fmt.Println(res)
 }
