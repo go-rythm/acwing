6c27ef350d465e8e878fc9018664572f832d15bf
luxcgo <luxcgo@gmail.com>
2021-11-08 01:57:30 +0800

    0906: 区间分组

---
+++ internal/oj/heap.go
@@ -0,0 +1,29 @@
+package oj
+
+// An MinHeap is a min-heap of ints.
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *MinHeap) Top() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	return x
+}
--- problems/0906/0906-区间分组.go
+++ problems/0906/0906-区间分组.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/luxcgo/go-acwing/internal/oj"
+)
+
+type pair struct {
+	l int
+	r int
+}
+
 func main() {
 
+	// in := oj.In()
+	// os.Stdin = in
+	// defer in.Close()
+
+	in := bufio.NewReader(os.Stdin)
+	var cnt int
+	fmt.Scan(&cnt)
+	pairs := make([]*pair, cnt)
+	for i := 0; i < cnt; i++ {
+		p := &pair{}
+		fmt.Fscan(in, &p.l, &p.r)
+		pairs[i] = p
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].l < pairs[j].l
+	})
+	minHeap := &oj.MinHeap{}
+	heap.Init(minHeap)
+	for _, p := range pairs {
+		if minHeap.Len() == 0 {
+			heap.Push(minHeap, p.r)
+			continue
+		}
+		top := heap.Pop(minHeap).(int)
+		if minHeap.Len() == 0 || p.l <= top {
+			heap.Push(minHeap, p.r)
+			heap.Push(minHeap, top)
+		} else {
+			heap.Push(minHeap, p.r)
+		}
+	}
+	fmt.Println(minHeap.Len())
 }
