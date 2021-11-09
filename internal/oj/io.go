0117b07e49b324e2564c15247830678ede3c154e
luxcgo <luxcgo@gmail.com>
2021-11-10 01:38:12 +0800

    oj: 增加定制scanner和memset

--- internal/oj/io.go
+++ internal/oj/io.go
@@ -1,6 +1,10 @@
 package oj
 
-import "os"
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
 func In() *os.File {
 	f, err := os.Open("in.txt")
@@ -8,4 +12,55 @@ func In() *os.File {
 		panic(err)
 	}
 	return f
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	s := &Scanner{
+		bufio.NewScanner(os.Stdin),
+	}
+	s.Split(bufio.ScanWords)
+	s.Buffer(make([]byte, 1024), int(1e9))
+	return s
+}
+
+func (s *Scanner) readString() string {
+	s.Scan()
+	return s.Text()
+}
+
+func (s *Scanner) readInt() int {
+	s.Scan()
+	i, err := strconv.Atoi(s.readString())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (s *Scanner) ReadString() string {
+	return s.readString()
+}
+
+func (s *Scanner) ReadInt() int {
+	return s.readInt()
+}
+
+func (s *Scanner) ReadStrings(n int) []string {
+	a := make([]string, n)
+	for i := 0; i < n; i++ {
+		a[i] = s.readString()
+	}
+	return a
+}
+
+func (s *Scanner) ReadInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = s.readInt()
+	}
+	return a
 }
--- internal/oj/math.go
+++ internal/oj/math.go
@@ -13,3 +13,13 @@ func Max(a, b int) int {
 	}
 	return b
 }
+
+func Memset(a []int, v int) {
+	if len(a) == 0 {
+		return
+	}
+	a[0] = v
+	for bp := 1; bp < len(a); bp *= 2 {
+		copy(a[bp:], a[:bp])
+	}
+}
