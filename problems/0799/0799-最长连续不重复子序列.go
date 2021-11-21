8fda9664cbd2fdd27f8724476ffebb14be7ef7b3
luxcgo <luxcgo@gmail.com>
2021-11-21 11:45:30 +0800

    0799: 双指针

--- problems/0799/0799-最长连续不重复子序列.go
+++ problems/0799/0799-最长连续不重复子序列.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/luxcgo/go-acwing/internal/oj"
+)
+
+const N = 100010
+
+var (
+	a []int
+	s [N]int
+)
+
+func main() {
+	in := oj.NewScanner()
+	n := in.ReadInt()
+	a = in.ReadInts(n)
+	var max int
+	for i, j := 0, 0; i < n; i++ {
+		s[a[i]]++
+		for s[a[i]] > 1 {
+			s[a[j]]--
+			j++
+		}
+		max = oj.Max(max, i-j+1)
+	}
+	fmt.Println(max)
 }
--- submit.go
+++ submit.go
@@ -7,40 +7,27 @@ import (
 	"strconv"
 )
 
-const N = 1010
+const N = 100010
 
 var (
-	a, b [N][N]int
+	a []int
+	s [N]int
 )
 
 func main() {
-	s := NewScanner()
-	n, m, q := s.ReadInt(), s.ReadInt(), s.ReadInt()
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= m; j++ {
-			a[i][j] = s.ReadInt()
-			insert(i, j, i, j, a[i][j])
+	in := NewScanner()
+	n := in.ReadInt()
+	a = in.ReadInts(n)
+	var max int
+	for i, j := 0, 0; i < n; i++ {
+		s[a[i]]++
+		for s[a[i]] > 1 {
+			s[a[j]]--
+			j++
 		}
+		max = Max(max, i-j+1)
 	}
-	for i := 0; i < q; i++ {
-		op := s.ReadInts(5)
-		insert(op[0], op[1], op[2], op[3], op[4])
-	}
-	// 对生成的差分矩阵求二维前缀和
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= m; j++ {
-			a[i][j] = a[i-1][j] + a[i][j-1] - a[i-1][j-1] + b[i][j]
-			fmt.Printf("%d ", a[i][j])
-		}
-		fmt.Println()
-	}
-}
-
-func insert(x1, y1, x2, y2, c int) {
-	b[x1][y1] += c
-	b[x1][y2+1] -= c
-	b[x2+1][y1] -= c
-	b[x2+1][y2+1] += c
+	fmt.Println(max)
 }
 
 /*
