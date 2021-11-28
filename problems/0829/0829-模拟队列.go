84e6eb7897fc5251066db9117ba5c0f3789829c7
luxcgo <luxcgo@gmail.com>
2021-11-28 18:21:04 +0800

    0829: 模拟队列

--- problems/0829/0829-模拟队列.go
+++ problems/0829/0829-模拟队列.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+const N = 100010
+
+var q [N]int
+
+// head 表示队头，tail表示队尾
+var head = 0
+var tail = -1
+
+// 向队尾插入一个数
+func push(x int) {
+	tail++
+	q[tail] = x
+}
+
+// 从队头弹出一个数
+func pop() {
+	head++
+}
+
+// 判断队列是否为空
+func isEmpty() bool {
+	return head > tail
+}
+
+// 队头的值
+func peek() int {
+	return q[head]
+}
+
+func main() {
+	var sb strings.Builder
+	var m int
+	fmt.Scan(&m)
+	for i := 0; i < m; i++ {
+		var op string
+		var x int
+		fmt.Scan(&op)
+		switch op {
+		case "push":
+			fmt.Scan(&x)
+			push(x)
+		case "pop":
+			pop()
+		case "empty":
+			if isEmpty() {
+				sb.WriteString("YES\n")
+			} else {
+				sb.WriteString("NO\n")
+			}
+		case "query":
+			fmt.Fprintf(&sb, "%d\n", peek())
+		}
+	}
+
+	fmt.Print(sb.String())
 }
