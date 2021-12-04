1a46b8f1a4184e0920a9a722097bc178f653efb1
luxcgo <luxcgo@gmail.com>
2021-12-05 00:00:52 +0800

    0840: 开放寻址法

--- problems/0840/0840-模拟散列表.go
+++ problems/0840/0840-模拟散列表.go
@@ -6,11 +6,63 @@ import (
 	"github.com/luxcgo/go-acwing/internal/oj"
 )
 
-const N = int(1e5 + 3)
+// // 方法一：拉链法
+// // 大于数据范围的第一个质数
+// const N = int(1e5 + 3)
+
+// var (
+// 	h, e, ne [N]int
+// 	idx      int
+// )
+
+// func main() {
+// 	s := oj.NewScanner()
+// 	n := s.ReadInt()
+
+// 	for i := 0; i < len(h); i++ {
+// 		h[i] = -1
+// 	}
+
+// 	for i := 0; i < n; i++ {
+// 		op := s.ReadString()
+// 		x := s.ReadInt()
+// 		switch op {
+// 		case "I":
+// 			insert(x)
+// 		case "Q":
+// 			if find(x) {
+// 				fmt.Println("Yes")
+// 			} else {
+// 				fmt.Println("No")
+// 			}
+// 		}
+// 	}
+// }
+
+// func insert(x int) {
+// 	k := (x%N + N) % N
+// 	e[idx] = x
+// 	ne[idx] = h[k]
+// 	h[k] = idx
+// 	idx++
+// }
+
+// func find(x int) bool {
+// 	k := (x%N + N) % N
+// 	for i := h[k]; i != -1; i = ne[i] {
+// 		if e[i] == x {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// 方法二：开放寻址法
+const N = int(2e5 + 3)
 
 var (
-	h, e, ne [N]int
-	idx      int
+	h    [N]int
+	null = 0x3f3f3f
 )
 
 func main() {
@@ -18,17 +70,18 @@ func main() {
 	n := s.ReadInt()
 
 	for i := 0; i < len(h); i++ {
-		h[i] = -1
+		h[i] = null
 	}
 
 	for i := 0; i < n; i++ {
 		op := s.ReadString()
 		x := s.ReadInt()
+		k := find(x)
 		switch op {
 		case "I":
-			insert(x)
+			h[k] = x
 		case "Q":
-			if find(x) {
+			if h[k] != null {
 				fmt.Println("Yes")
 			} else {
 				fmt.Println("No")
@@ -37,20 +90,13 @@ func main() {
 	}
 }
 
-func insert(x int) {
+func find(x int) int {
 	k := (x%N + N) % N
-	e[idx] = x
-	ne[idx] = h[k]
-	h[k] = idx
-	idx++
-}
-
-func find(x int) bool {
-	k := (x%N + N) % N
-	for i := h[k]; i != -1; i = ne[i] {
-		if e[i] == x {
-			return true
+	for h[k] != null && h[k] != x {
+		k++
+		if k == N {
+			k = 0
 		}
 	}
-	return false
+	return k
 }
